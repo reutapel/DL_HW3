a65c55730f02ecdd764567eb2cfa8eee6de8578c
reutapel <reutapel88@gmail.com>
2017-01-15 21:39:05 +0200

    Update PennTreebankLSTM.lua

--- PennTreebankLSTM.lua
+++ PennTreebankLSTM.lua
@@ -3,7 +3,7 @@ require 'nn'
 require 'optim'
 require 'eladtools'
 require 'recurrent'
-require 'textDataProvider'
+require 'utils.textDataProvider'
 -------------------------------------------------------
 
 cmd = torch.CmdLine()
