bf506ec012f884baa2fb2c55ace4c2e33394e601
SHIMONSHEIBA <shimonsheiba@gmail.com>
2017-01-21 20:27:07 +0200

    Update PenLSTM_Sample.lua

--- PenLSTM_Sample.lua
+++ PenLSTM_Sample.lua
@@ -130,7 +130,7 @@ repeat
 
   local LossTest = evaluate(data.testData)
 
-  print('\nSampled Text:\n' .. sample('the meaning of life is', 50, true))
+  print('\nSampled Text:\n' .. sample('Buy low, sell high is the', 10, true))
 
   print('\nTest Perplexity: ' .. torch.exp(LossTest))
   log:add{['Training Loss']= LossTrain, ['Validation Loss'] = LossVal, ['Test Loss'] = LossTest}
