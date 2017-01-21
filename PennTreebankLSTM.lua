52876bef202e4b86dd0772074f474db5469106c5
reutapel <reutapel88@gmail.com>
2017-01-21 19:21:51 +0200

    Update PennTreebankLSTM.lua

--- PennTreebankLSTM.lua
+++ PennTreebankLSTM.lua
@@ -21,7 +21,7 @@ cmd:option('-model',              'LSTM',                      'Recurrent model 
 cmd:option('-seqLength',          10,                          'number of timesteps to unroll for')
 cmd:option('-rnnSize',            256,                         'size of rnn hidden layer')
 cmd:option('-numLayers',          2,                           'number of layers in the LSTM')
-cmd:option('-dropout',            1,                           'dropout p value')
+cmd:option('-dropout',            0,                           'dropout p value')
 cmd:option('-LR',                 2e-4,                        'learning rate')
 cmd:option('-LRDecay',            0,                           'learning rate decay (in # samples)')
 cmd:option('-weightDecay',        0,                           'L2 penalty on the weights')
