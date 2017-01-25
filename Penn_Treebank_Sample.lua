245063c9b941c3069ad0f69d009ff4225dde015e
SHIMONSHEIBA <shimonsheiba@gmail.com>
2017-01-25 15:43:16 +0200

    Update Penn_Treebank_Sample.lua

--- Penn_Treebank_Sample.lua
+++ Penn_Treebank_Sample.lua
@@ -18,7 +18,7 @@ cmd:option('-shuffle',            false,                       'shuffle training
 
 cmd:text('===>Model And Training Regime')
 cmd:option('-model',              'LSTM',                      'Recurrent model [RNN, iRNN, LSTM, GRU]')
-cmd:option('-seqLength',          50,                          'number of timesteps to unroll for')
+cmd:option('-seqLength',          15,                          'number of timesteps to unroll for')
 cmd:option('-rnnSize',            220,                         'size of rnn hidden layer')
 cmd:option('-numLayers',          2,                           'number of layers in the LSTM')
 cmd:option('-dropout',            0.2,                         'dropout p value')
@@ -32,7 +32,7 @@ cmd:option('-initWeight',         0.08,                        'uniform weight i
 cmd:option('-earlyStop',          30,                        'number of bad epochs to stop after')
 cmd:option('-optimization',       'rmsprop',                   'optimization method')
 cmd:option('-gradClip',           5,                           'clip gradients at this value')
-cmd:option('-epoch',              100,                         'number of epochs to train')
+cmd:option('-epoch',              2,                         'number of epochs to train')
 cmd:option('-epochDecay',         25,                           'number of epochs to start decay learning rate')
 
 cmd:text('===>Platform Optimization')
@@ -162,14 +162,14 @@ print("Best Iteration was " .. bestIteration .. ", With a validation loss of: " 
 --log:style{['Best Iteration was'] = '-', ['With a validation loss of'] = '-'}
 --log:plot()
 
---if paths.filep(opt.load) then
---    modelConfig = torch.load(opt.load)
---    print('==>Loaded Net from: ' .. opt.load)
---    numOfSentences = 5
---    for i=1, numOfSentences do
---      print('\nSampled Text:\n' .. sample('Buy low, sell high is the', 5, true))
---    end
---end
+if paths.filep(opt.load) then
+    modelConfig = torch.load(opt.load)
+    print('==>Loaded Net from: ' .. opt.load)
+    numOfSentences = 5
+    for i=1, numOfSentences do
+      print('\nSampled Text:\n' .. sample('Buy low, sell high is the', 5, true))
+    end
+end
     
 
 require 'gnuplot'
