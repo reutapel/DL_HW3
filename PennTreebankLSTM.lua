70fa5a50de8301f243ff17500876dddd03dcc5a4
reutapel <reutapel88@gmail.com>
2017-01-25 23:43:49 +0200

    Update PennTreebankLSTM.lua

--- PennTreebankLSTM.lua
+++ PennTreebankLSTM.lua
@@ -32,7 +32,7 @@ cmd:option('-initWeight',         0.08,                        'uniform weight i
 cmd:option('-earlyStop',          100,                         'number of bad epochs to stop after')
 cmd:option('-optimization',       'rmsprop',                   'optimization method')
 cmd:option('-gradClip',           5,                           'clip gradients at this value')
-cmd:option('-epoch',              1,                         'number of epochs to train')
+cmd:option('-epoch',              2,                         'number of epochs to train')
 cmd:option('-epochDecay',         25,                           'number of epochs to start decay learning rate')
 
 cmd:text('===>Platform Optimization')
